93336eab7b40d6fc6e0663396b839da4d669f25b
amansethi41 <sethi.rva139@gmail.com>
2026-05-28 08:50:12 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "aman"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// numder => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (empty)
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
