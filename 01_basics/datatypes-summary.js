62c152023ae8d4df1bcf69c06a303874575872ea
amansethi41 <sethi.rva139@gmail.com>
2026-05-31 07:57:26 +0000

    container config done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+//  Primitive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const sccore = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 2632736274674627n
+
+// n means bigInt
+
+//  Reference (Non primitive)
+
+//  Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "tinku"]
+
+let myObj = {
+name: "Aman",
+age: 21,    
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof myFunction);
